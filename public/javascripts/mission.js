9e848655ff9edbe57560c60eada3905d6ea16c82
Nohyunji <t4933@naver.com>
2024-07-23 18:01:09 +0900

    포인트 확인 및 잔액체크로직 추가

--- public/javascripts/mission.js
+++ public/javascripts/mission.js
@@ -238,7 +238,7 @@ async function attendance() {
   }
 
   var signature;
-  var contractAddr = "0x9d42388a4141440e02dc36c415e2045a64a5af76";
+  var contractAddr = "0x44CfB2fEa7361d299D32d9Ac579FD9640eB5F0Ba";
   try {
     web3 = new Web3(web3.currentProvider);
   } catch (error) {
@@ -246,6 +246,19 @@ async function attendance() {
       "https://chromewebstore.google.com/detail/metamask/nkbihfbeogaeaoehlefnkodbefgpgknn?hl=ko&pli=1"
     );
 
+    return;
+  }
+
+  // 잔액 확인 추가
+  const account = ethereum.selectedAddress;
+  const balance = await web3.eth.getBalance(account);
+  const minBalance = web3.utils.toWei("0.001", "ether");
+
+  if (balance < minBalance) {
+    $("#confirm").css("display", "block");
+    $("#confirm .title").append(
+      "Insufficient balance in MetaMask. Please add more funds."
+    );
     return;
   }
 
@@ -295,7 +308,7 @@ async function attendance() {
         ],
       },
       domain: {
-        name: "1.234.112.72",
+        name: "211.215.74.85",
         version: "1",
         chainId: chainId,
         verifyingContract: contractAddr, // 사용할 컨트랙트 주소 입력 마켓컨트랙트, 라우터컨트랙트
